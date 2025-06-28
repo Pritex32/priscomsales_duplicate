9ebc4c104ab865715256ae005b7be2d804ca24db
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2025-06-28 16:14:22 -0700

    Update Dashboard.py

--- chibuzo_sales/pages/Dashboard.py
+++ chibuzo_sales/pages/Dashboard.py
@@ -740,12 +740,12 @@ if st.session_state.get("logged_in") and st.session_state.get("role") == "md":
 import requests
 
 # Use your Paystack **SECRET** key here (starts with sk_live...) — not the PUBLIC key
-paystack_key = os.getenv("PAYSTACK_SECRET_KEY")  # this is to hide the real key from public eyes
+PAYSTACK_SECRET_KEY = "sk_test_b4d6bbad64ad55d65008808f70cf8ba74ff830d7"   # this is to hide the real key from public eyes
 CALLBACK_URL = "https://priscomac-com.onrender.com//payment-success"  # Optional, can be your app URL
 
 def initialize_payment(email, amount, user_id):
     headers = {
-        "Authorization": f"Bearer {paystack_key"},
+        "Authorization": f"Bearer {PAYSTACK_SECRET_KEY},
         "Content-Type": "application/json"
     }
     
