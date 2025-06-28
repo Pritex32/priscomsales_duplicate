b5e94329f8d59ae506ca3c091bff34defe8219a3
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2025-06-28 16:17:42 -0700

    Update Dashboard.py

--- chibuzo_sales/pages/Dashboard.py
+++ chibuzo_sales/pages/Dashboard.py
@@ -745,7 +745,7 @@ CALLBACK_URL = "https://priscomac-com.onrender.com//payment-success"  # Optional
 
 def initialize_payment(email, amount, user_id):
     headers = {
-        "Authorization": f"Bearer {PAYSTACK_SECRET_KEY},
+        "Authorization": f"Bearer {PAYSTACK_SECRET_KEY}",
         "Content-Type": "application/json"
     }
     
