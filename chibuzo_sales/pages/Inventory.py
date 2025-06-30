cfa7e00dc180e9a67eee177fe4ae06ac13b18a33
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2025-06-30 06:27:19 -0700

    Update Inventory.py

--- chibuzo_sales/pages/Inventory.py
+++ chibuzo_sales/pages/Inventory.py
@@ -25,6 +25,33 @@ from streamlit_javascript import st_javascript
 from datetime import datetime, timedelta
 import jwt
 
+
+
+# to show spiner rotating when the app is laoding
+st.markdown("""
+    <style>
+    .loader {
+      border: 6px solid #f3f3f3;
+      border-top: 6px solid #00FFC6;
+      border-radius: 50%;
+      width: 50px;
+      height: 50px;
+      animation: spin 1s linear infinite;
+      margin: auto;
+      position: relative;
+      top: 50px;
+    }
+
+    @keyframes spin {
+      0% { transform: rotate(0deg); }
+      100% { transform: rotate(360deg); }
+    }
+    </style>
+
+    <div class="loader"></div>
+    <h5 style="text-align:center;">Loading Priscomac App...</h5>
+""", unsafe_allow_html=True)
+
 jwt_SECRET_KEY = "4606"  # Use env vars in production
 ALGORITHM = "HS256"
 
