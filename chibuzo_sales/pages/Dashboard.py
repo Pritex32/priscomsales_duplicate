58001b900d2c6aa29633b543e3ecb556deef7b2e
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2025-07-15 18:05:56 -0700

    Update Dashboard.py

--- chibuzo_sales/pages/Dashboard.py
+++ chibuzo_sales/pages/Dashboard.py
@@ -1014,9 +1014,9 @@ if "user_id" not in st.session_state or "user_email" not in st.session_state:
 user_id = st.session_state["user_id"]
 email = st.session_state["user_email"]
 
-if st.button("Upgrade to Pro (₦10000)"):
+if st.button("Upgrade to Pro (₦1000)"):
     st.info('Upgrade to 1 month plan,you will have complete access to all your sales data.')
-    result = initialize_payment(email, 10000, user_id)
+    result = initialize_payment(email, 1000, user_id)
     
     if result.get("status") and "data" in result:
         auth_url = result["data"].get("authorization_url")
