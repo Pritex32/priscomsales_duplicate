9a9eb588a22b356590a35f4e57e7d20b48df13ef
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2025-07-19 09:17:06 -0700

    Update Dashboard.py

--- chibuzo_sales/pages/Dashboard.py
+++ chibuzo_sales/pages/Dashboard.py
@@ -173,11 +173,24 @@ def save_token_to_localstorage(token):
 
 from supabase import create_client
 # supabase configurations
+@st.cache_resource
 def get_supabase_client():
     supabase_url = 'https://ecsrlqvifparesxakokl.supabase.co' # Your Supabase project URL
     supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjc3JscXZpZnBhcmVzeGFrb2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NjczMDMsImV4cCI6MjA2MDI0MzMwM30.Zts7p1C3MNFqYYzp-wo3e0z-9MLfRDoY2YJ5cxSexHk'
-    supabase = create_client(supabase_url, supabase_key)
-    return supabase  # Make sure to return the client
+    try:
+        supabase = create_client(supabase_url, supabase_key)
+       
+        return supabase
+
+    except Exception as e:
+        st.error("❌ Failed to connect to database. Please check your internet or try again later.")
+        # Optional: Print or log error for debugging during development
+        # st.write(e)
+        st.stop()
+    
+# Initialize Supabase client
+supabase = get_supabase_client() # use this to call the supabase database
+ # Make sure to return the client
 
 # Initialize Supabase client
 supabase = get_supabase_client() # use this to call the supabase database
