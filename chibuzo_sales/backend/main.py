b8d96ea9299c98572cc9e82b6f67cd0366aee5c1
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2025-07-25 10:31:48 -0700

    Update main.py

--- chibuzo_sales/backend/main.py
+++ chibuzo_sales/backend/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-from routers import sales, restock
+from routers import sales, restock,expenses, payments
 
 app = FastAPI(title="PriscomSales API")
 
 # Include Routers
 app.include_router(sales.router, prefix="/api/sales", tags=["sales"])
 app.include_router(restock.router, prefix="/api/restock", tags=["restock"])
+app.include_router(expenses.router, prefix="/api/expenses", tags=["Expenses"])
+app.include_router(payments.router, prefix="/api/payments", tags=["Payments"])
+
 
