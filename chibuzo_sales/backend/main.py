e21c50482ad75ff5dd4cc6db7b1b1bd0fbce6407
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2025-07-25 09:38:26 -0700

    Update main.py

--- chibuzo_sales/backend/main.py
+++ chibuzo_sales/backend/main.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+from routers import sales, restock
+
+app = FastAPI(title="ChibuSales API")
+
+# Include Routers
+app.include_router(sales.router, prefix="/api/sales", tags=["Sales"])
+app.include_router(restock.router, prefix="/api/restock", tags=["Restock"])
 
