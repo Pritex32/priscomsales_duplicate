53fe17540cf038e2bfd1db6b5df57f01a85e720e
Prisca Ukanwa <oluomachiukanwa32@gmail.com>
2025-08-03 11:19:24 -0700

    Update Create_Sheet.py

--- chibuzo_sales/pages/Create_Sheet.py
+++ chibuzo_sales/pages/Create_Sheet.py
@@ -104,6 +104,10 @@ def decode_jwt(token):
     
 
 # Restore login from browser localStorage
+def handle_session_expiration():
+    st.session_state["logged_in"] = False
+    st.session_state["session_expired"] = True
+
 
 # === Restore Login from JWT ===
 def restore_login_from_jwt():
@@ -133,15 +137,6 @@ def restore_login_from_jwt():
                 st_javascript("""localStorage.removeItem("login_token");""")
                 st.session_state.login_failed = True
 
-
-# Run this first
-
-# === Session Validation ===
-# === Session Validation === # this stops you when you are logged out
-def handle_session_expiration():
-    st.session_state["logged_in"] = False
-    st.session_state["session_expired"] = True
-    st.rerun()# or redirect logic
 
 restore_login_from_jwt()
 
